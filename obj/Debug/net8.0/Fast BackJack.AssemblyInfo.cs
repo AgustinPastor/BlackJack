af1113279242acd8af9a5b67340caa6b5e2edf3d
AgustinPastor <agus.pastman@gmail.com>
2025-02-07 13:25:17 +0100

    Actualización

--- obj/Debug/net8.0/Fast BackJack.AssemblyInfo.cs
+++ obj/Debug/net8.0/Fast BackJack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fast BackJack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0e2128b69ba30b5b5c8c3295b4fffc5a3d053d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fast BackJack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fast BackJack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
